4d4f8f9d8f4f295f9f438eacdf6e523a55aa0d4d
SaintKyou <49764769+SaintKyou@users.noreply.github.com>
2021-09-20 04:38:34 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -22,9 +22,9 @@ void loop(unsigned int start, unsigned int end)
 //   }
 //}
 
-int main (int argc, char **argv)
+int main ()
 {
-  std::ifstream f1("C:\\Users\\ASUS Zephyrus\\Desktop\\test\\2\\cors_out.txt");
+  std::ifstream f1("C:\\Users\\ASUS Zephyrus\\Desktop\\test\\cors_out.txt");
   constexpr int N = 897117;
   int* id = new int[N];
   double* p = new double[N];
@@ -33,25 +33,28 @@ int main (int argc, char **argv)
   Generator par(60, 2, 64, 1.46, 6.0);
 
   //x0,y0,Ns,Nn,Mm,M,(jd(i),ien(i),nedmu(i),i=1,64),& sEn,ECR,THETACR,PHICR,Mh0
-  par.randomcp();
-  for(int i = 0; i < N; ++i){
-      f1 >> id[i] >> p[i] >> x[i] >> y[i];
 
-      par.set(&id[i], &p[i], &x[i], &y[i]);
-      par.process();
-      //par.output()
-
+  int throws{4255}, ShowerCounter{};
+//  std::cout << "Бросков: " << '\n';
+//  std::cin >> throws;
+//  std::cout << "Бросков " << throws << std::endl;
+  for(int j = 0; j < throws; ++j){
+    ++ShowerCounter;
+    par.randomcp();
+    for(int i = 0; i < N; ++i){
+        f1 >> id[i] >> p[i] >> x[i] >> y[i];
+        par.set(&id[i], &p[i], &x[i], &y[i]);
+        par.process();
+    }
+    par.output(ShowerCounter, 500000, 0, 0);
   }
-  std::cout << par.x0 << ' ' << par.y0 << endl;
+
+  //std::cout << par.x0 << ' ' << par.y0 << endl;
   //for(int i = 0; i < 64; ++i) std::cout << par.ed[i] << ',';
   //std::cout << '\n';
 
   //std::cout << par.Nn << ' ' << par.Mm << ' ' << par.ed[0] << endl;;
-  par.output(1, 500000, 0, 0);
-
-
-
-  //std::cout << id[1] << ' ' << p[1] << ' ' << x[1] << ' ' << y[1] << endl;
+  //par.output(1, 500000, 0, 0);
 
 
   return 0;
