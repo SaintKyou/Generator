992cc865516a782585c55e46109dc0d7e4b6b296
SaintKyou <49764769+SaintKyou@users.noreply.github.com>
2021-12-01 22:07:27 +0300

    Update generator.h

--- generator.h
+++ generator.h
@@ -7,36 +7,22 @@
 class Generator
 {
 private:
-    double *detx = new double[64];
-    double *dety = new double[64];
-    void fill_coord(){                                  // заполнение координат детекторов
-        for(int i = 0; i < 16; ++i)
-        {
-            detx[i] = (-4 + 0.5 + i%4)*5;
-            detx[i+16] = detx[i] + 20;
-            detx[i+32] = detx[i];
-            detx[i+48] = detx[i+16];
-
-            dety[i] = (4 - 0.5 - i/4)*5;
-            dety[i+16] = dety[i];
-            dety[i+32] = dety[i] - 20;
-            dety[i+48] = dety[i+32];
-        }
-    }
-    int radius{}, krat{}, Number_det{};                 //60, 2, 64
-    float par_per_channel{}, threshold{};               //  1.46, 6.0 2.44   !   2.44*3/5=1.46        ! particles per channel of ADC in MSC
+    double detx[16];
+    double dety[16];
+    int radius, krat, Number_det;                 //60, 1, 16
+    float par_per_channel, threshold;             //  1.46, 6.0 2.44   !   2.44*3/5=1.46        ! particles per channel of ADC in MSC
     // Corsika
-    int pid{};
-    double pie{}, pix{}, piy{};
+    int pid;
+    double pie, pix, piy;
 public:
-    Generator()
-    {
-        radius = 60;
-        krat = 2;
-        Number_det = 64;
-        par_per_channel = 1.46;
-        threshold = 6.0;
-    }
+//    Generator()
+//    {
+//        radius = 60;
+//        krat = 2;
+//        Number_det = 64;
+//        par_per_channel = 1.46;
+//        threshold = 6.0;
+//    }
     Generator(int _radius, int _krat, int _Number_det, float _par_per_channel, float _threshold)
     {
     radius = _radius;
@@ -44,18 +30,19 @@ public:
     Number_det = _Number_det;
     par_per_channel = _par_per_channel;
     threshold = _threshold;
-    fill_coord();
+    std::ifstream conf("config.txt");
+    for(int i = 0; i < 16; ++i) conf >> detx[i] >> dety[i];
+    Number_muons = Number_hadrons_circle = Number_hadrons_core = Number_neutrons_circle = Number_neutrons = 0;
+    E_Gev = Y_shift = X_shift = Number_event = Ne = 0;
+    for(int i = 0; i < 16; ++i) en_deposit[i] = muon_det[i] = integ_par[i] = 0;
     }
     ~Generator() {}
-    double x_core{}, y_core{};
-    int Number_muons{}, Number_hadrons_circle{}, Number_hadrons_core{}, Number_neutrons_circle{}, Number_neutrons{};
-    double E_Gev{}, Y_shift{}, X_shift{}, Number_event{}, Ne{};
-    double *en_deposit = new double[64];
-    double *muon_det = new double[64];
-    double *integ_par = new double[64];
-
-    //double *number_par_adc = new double[64];
-
+    double x_core, y_core;
+    int Number_muons, Number_hadrons_circle, Number_hadrons_core, Number_neutrons_circle, Number_neutrons /*, M*/;
+    double E_Gev, Y_shift, X_shift, Number_event, Ne /*, lg_Ne{}*/;
+    double en_deposit[16];
+    double muon_det[16];
+    double integ_par[16];
     std::vector <double> all;
     void set(int *pid1, double *pie1, double *pix1, double *piy1){
         pid = *pid1;
