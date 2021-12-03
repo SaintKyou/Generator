47abe4b28689f49bf85d6d46335a4897bdafd972
SaintKyou <49764769+SaintKyou@users.noreply.github.com>
2021-12-03 21:23:38 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
-#include <future>
 #include <thread>
-#include <random>
 #include <ctime>
 #include <cmath>
+#include <mutex>
 #include "generator.h"
-#include <future>
-#include <memory>
-#include <list>
 
+int ShowerCounter{};
 constexpr int N = 897117;
 int* id = new int[N];
 double* p = new double[N];
 double* x = new double[N];
 double* y = new double[N];
-std::ifstream f1("C:\\Users\\ASUS Zephyrus\\Desktop\\test\\cors_out.txt");
+std::ifstream f2("C:\\Users\\ASUS Zephyrus\\Desktop\\test\\cors_out.txt");
+
+std::mutex g_lock;
 
 void loop3(int number)
 {
-    Generator par2(60, 2, 64, 1.46, 6.0);
+    Generator par2(60, 1, 16, 1.46, 6.0);
     for(unsigned int k = 0; k < number; ++k){
         //++ShowerCounter;
         par2.randomcp();
@@ -29,17 +28,18 @@ void loop3(int number)
             }
         par2.output(k, 500000, 0, 0);
         }
+    g_lock.lock();
     par2.print_all();
+    g_lock.unlock();
 }
 
 std::vector<std::thread> thread_pool;
-//std::list<std::future<void>> taskList;
 
 int main ()
 {
-  for(int i = 0; i < N; ++i) f1 >> id[i] >> p[i] >> x[i] >> y[i];
-  int throws{5000};
-  int number_of_threads = 16;
+  for(int i = 0; i < N; ++i) f2 >> id[i] >> p[i] >> x[i] >> y[i];
+  int throws{1000};
+  int number_of_threads = 8;
   int step = throws/number_of_threads; // std::thread::hardware_concurrency()
 
   for(int i = 0; i < number_of_threads; ++i){
@@ -48,10 +48,10 @@ int main ()
 
   }
 
-  for(auto& entry: thread_pool)
+  for(int i = 0; i < number_of_threads; ++i){
+      std::thread& entry = thread_pool[i];
       entry.join();
-
-//loop3(throws);    // 1 thread
+  }
 
   return 0;
 }
