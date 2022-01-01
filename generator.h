00f3bea783bd974b56580665db53759cac2fa8b9
SaintKyou <49764769+SaintKyou@users.noreply.github.com>
2022-01-01 15:30:52 +0300

    Update generator.h

--- generator.h
+++ generator.h
@@ -14,11 +14,12 @@ private:
 //    double *dety = new double[64];
     double detx[16];
     double dety[16];
-    int radius, krat, Number_det;                 //60, 2, 64
-    float par_per_channel, threshold;               //  1.46, 6.0 2.44   !   2.44*3/5=1.46        ! particles per channel of ADC in MSC
+    int krat, Number_det;                 // 2, 64
+    float par_per_channel, threshold, radius;               //  1.46, 6.0 2.44   !   2.44*3/5=1.46        ! particles per channel of ADC in MSC
     // Corsika
-    int pid;
+    int pid, pit;
     double pie, pix, piy;
+    std::vector<std::vector<int>> times_det;
 public:
 //    Generator()
 //    {
@@ -31,16 +32,17 @@ public:
     std::tr1::random_device rd;
     typedef std::tr1::mt19937 MyRng;
     MyRng gen;
-    Generator(int _radius, int _krat, int _Number_det, float _par_per_channel, float _threshold): gen(rd())
+    Generator(double _radius, int _krat, int _Number_det, float _par_per_channel, float _threshold, unsigned int _seed): times_det(16, std::vector<int>()), gen(rd())
     {
+    gen.seed(static_cast<std::tr1::mt19937::result_type>(_seed));
     //gen.seed(static_cast<std::tr1::mt19937::result_type>(::time(NULL)));
-    gen.seed(static_cast<std::tr1::mt19937::result_type>(std::hash<std::thread::id>()(std::this_thread::get_id())));
+    //gen.seed(static_cast<std::tr1::mt19937::result_type>(std::hash<std::thread::id>()(std::this_thread::get_id())));
     radius = _radius;
     krat = _krat;
     Number_det = _Number_det;
     par_per_channel = _par_per_channel;
     threshold = _threshold;
-    std::ifstream conf("config.txt");
+    std::ifstream conf("C:\\Qt\\progects\\e_dep_clust\\test\\config_rect.txt");
     for(int i = 0; i < 16; ++i) conf >> detx[i] >> dety[i];
 /*    for(int i = 0; i < 16; ++i)
     {
@@ -57,7 +59,7 @@ public:
 
     Number_muons = Number_hadrons_circle = Number_hadrons_core = Number_neutrons_circle = Number_neutrons = 0;
     E_Gev = Y_shift = X_shift = Number_event = Ne = 0;
-    for(int i = 0; i < 16; ++i) en_deposit[i] = muon_det[i] = integ_par[i] = 0;
+    for(int i = 0; i < 16; ++i) en_deposit[i] = muon_det[i] = integ_par[i] = /*k_index[i] = time_trig[i]=*/ 0;
     //fill_coord();
     }
     ~Generator() {}
@@ -73,17 +75,24 @@ public:
 
     //double *number_par_adc = new double[64];
 
+//    std::vector <int> t1;
+//    int time_trig[16];
+//    int time_det1 = 0;
+//    int t[16][5];
+//    int k_index[16];
+
     std::vector <double> all;
-    void set(int *pid1, double *pie1, double *pix1, double *piy1){
+    void set(int *pid1, double *pie1, double *pix1, double *piy1, int *pit1){
         pid = *pid1;
         pie = *pie1;
         pix = *pix1;
         piy = *piy1;
+        pit = *pit1;
     }
     void set_seed(int);
     double uniform(int, int/*, std::tr1::mt19937*/);
     int poisson(double);
-    void print_all();
+    void print_all(/*std::string*/);
     double gamma_dep_inside(double&);
     double gamma_dep_outside(double&);
     double electrons_dep_inside(double&);
@@ -100,3 +109,4 @@ public:
 };
 
 #endif // GENERATOR_H
+
