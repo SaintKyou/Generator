26b091d527a3ebfc78c7d80efc65ce54870fb18b
SaintKyou <49764769+SaintKyou@users.noreply.github.com>
2022-01-13 15:19:25 +0300

    Update generator.h

--- generator.h
+++ generator.h
@@ -10,11 +10,9 @@
 class Generator
 {
 private:
-//    double *detx = new double[64];
-//    double *dety = new double[64];
     double detx[16];
     double dety[16];
-    int krat, Number_det;                 //60, 2, 64
+    int krat, Number_det;                                   //60, 2, 64
     float par_per_channel, threshold, radius;               //  1.46, 6.0 2.44   !   2.44*3/5=1.46        ! particles per channel of ADC in MSC
     // Corsika
     unsigned short int pid;
@@ -22,22 +20,12 @@ private:
     std::vector<std::vector<int>> times_det;
     std::vector<int> time_trig;
 public:
-//    Generator()
-//    {
-//        radius = 60;
-//        krat = 2;
-//        Number_det = 64;
-//        par_per_channel = 1.46;
-//        threshold = 6.0;
-//    }
     std::tr1::random_device rd;
     typedef std::tr1::mt19937 MyRng;
     MyRng gen;
     Generator(float _radius, int _krat, int _Number_det, float _par_per_channel, float _threshold, unsigned int _seed): times_det(16, std::vector<int>()), gen(rd())
     {
     gen.seed(static_cast<std::tr1::mt19937::result_type>(_seed));
-    //gen.seed(static_cast<std::tr1::mt19937::result_type>(std::hash<std::thread::id>()(std::this_thread::get_id())));
-    //gen.seed();
     radius = _radius;
     krat = _krat;
     Number_det = _Number_det;
@@ -49,20 +37,14 @@ public:
     Number_muons = Number_hadrons_circle = Number_hadrons_core = Number_neutrons_circle = Number_neutrons = 0;
     E_Gev = Y_shift = X_shift = Number_event = Ne = 0;
     for(int i = 0; i < 16; ++i) en_deposit[i] = muon_det[i] = integ_par[i] = 0;
-    //fill_coord();
     }
     ~Generator() {}
     double x_core, y_core;
     int Number_muons, Number_hadrons_circle, Number_hadrons_core, Number_neutrons_circle, Number_neutrons /*, M*/;
     double E_Gev, Y_shift, X_shift, Number_event, Ne /*, lg_Ne{}*/;
-//    double *en_deposit = new double[64];
-//    double *muon_det = new double[64];
-//    double *integ_par = new double[64];
     double en_deposit[16];
     double muon_det[16];
     double integ_par[16];
-
-    //double *number_par_adc = new double[64];
 
     std::vector <double> all;
     void set(unsigned short int *pid1, int *pie1, int *pix1, int *piy1, int *pit1){
@@ -72,8 +54,7 @@ public:
         piy = *piy1;
         pit = *pit1;
     }
-    //void set_seed(int);
-    double uniform(int, int/*, std::tr1::mt19937*/);
+    double uniform(int, int);
     int poisson(double);
     void print_all();
     double gamma_dep_inside(double&);
